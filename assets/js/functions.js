e0f21f0bd2f70ba3dad0bcd012b279e908da5090
Courtney-L <cw.logan@gmail.com>
2020-09-20 20:13:34 +0000

    Feat: Added button event listener function for Service Card button Book Now -- Assets > Js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,31 @@
+// --------------------------------------------- Service Function
+
+// Set Button Variable(s)
+var service_01_btn = document.getElementById("service_01");
+var service_02_btn = document.getElementById("service_02");
+var service_03_btn = document.getElementById("service_03");
+var splash_btn = document.getElementById("splash_btn");
+
+function service_01(){
+    document.getElementById("bookingService").value = document.getElementById("service_01").value;
+}
+
+function service_02(){
+    document.getElementById("bookingService").value = document.getElementById("service_02").value;
+}
+
+function service_03(){
+    document.getElementById("bookingService").value = document.getElementById("service_03").value;
+}
+
+// Button Event Handler
+service_01_btn.onclick = service_01;
+service_02_btn.onclick = service_02;
+service_03_btn.onclick = service_03;
+splash_btn.onclick = service_02;
+
+// --------------------------------------------- End-of-function
+
 // --------------------------------------------- Automatic Slideshow function
 
 var slideIndex = 0;
